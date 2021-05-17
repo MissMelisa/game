b8ceddd92116b7c099dbb5be7968415f7c35035f
MissMelisa <melissarios497@gmail.com>
2021-05-17 11:56:23 -0300

    create main page and add player 1 and 2 selected options

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Game from "./Components";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Rock, paper, scissor</h1>
+      <div>
+        <Game round={setCounter} playerNumber="1" />
+      </div>
     </div>
   );
 }
